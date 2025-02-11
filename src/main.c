5b7a2383415d68127a052a4818584fd6a81644ba
Joao Marcos Souza Fernandes <jo0209.letras@gmail.com>
2025-02-11 17:55:08 -0300

    Adding the 'Add Block during the simulation' feature

--- src/main.c
+++ src/main.c
@@ -1,8 +1,5 @@
 #include <raylib.h>
 
-#define DELTA_TIME(FPS) (float)(1.0 / (float) FPS)
-#define BLOCK_SIZE 80.0
-
 void RandomizeColor(Color* color) {
 
 	color->r = GetRandomValue(0, 255);
@@ -11,58 +8,106 @@ void RandomizeColor(Color* color) {
 
 }
 
+Color GetRandomColor() {
+
+	Color color;
+
+	color.r = GetRandomValue(0, 255);
+	color.g = GetRandomValue(0, 255);
+	color.b = GetRandomValue(0, 255);
+
+	return color;
+}
+
+typedef struct {
+
+	Rectangle rect;
+
+	float velocity_x;
+	float velocity_y;
+
+	Color color;
+
+} Block;
+
+void InitBlock(Block* block, int window_width, int window_height) {
+
+	float size = (float)GetRandomValue(50, 80);
+	float posx = (float)GetRandomValue(0, (int)(window_width - size));
+	float posy = (float)GetRandomValue(0, (int)(window_height - size));
+
+	float velocity_x = (float)GetRandomValue(300, 800);
+	float velocity_y = (float)GetRandomValue(300, 800);
+
+	*(block) = (Block){
+		.rect = { posx, posy, size, size },
+		.velocity_x = velocity_x,
+		.velocity_y = velocity_y,
+		.color = GetRandomColor(),
+	};
+
+}
+
+void UpdateBlock(Block* block, int window_width, int window_height, float dt) {
+
+	block->rect.x += (block->velocity_x * dt);
+	block->rect.y += (block->velocity_y * dt);
+
+	if (block->rect.x + block->rect.width > window_width || block->rect.x < 0) {
+		block->velocity_x *= -1;
+		RandomizeColor(&block->color);
+	}
+
+	if (block->rect.y + block->rect.height > window_height || block->rect.y < 0) {
+		block->velocity_y *= -1;
+		RandomizeColor(&block->color);
+	}
+
+}
+
+void RenderBlock(Block* block) {
+	DrawRectangleRec(block->rect, block->color);
+}
+
 int main() {
 
 	const char* Window_title = "BlockSimulation";
 
-	SetConfigFlags(FLAG_FULLSCREEN_MODE | FLAG_BORDERLESS_WINDOWED_MODE | FLAG_VSYNC_HINT);
+	SetConfigFlags(FLAG_FULLSCREEN_MODE | FLAG_VSYNC_HINT);
 	InitWindow(0, 0, Window_title);
 
 	int fps = GetMonitorRefreshRate(GetCurrentMonitor());
-	int Window_width = GetMonitorWidth(GetCurrentMonitor());
-	int Window_height = GetMonitorHeight(GetCurrentMonitor());
+	int window_width = GetMonitorWidth(GetCurrentMonitor());
+	int window_height = GetMonitorHeight(GetCurrentMonitor());
 
-	Rectangle rect = {
-		.x = 0.0,
-		.y = 0.0,
-		.width = BLOCK_SIZE,
-		.height = BLOCK_SIZE,
-	};
+	SetTargetFPS(fps);
 
-	float velocity_x = 600;
-	float velocity_y = 600;
+	int block_lenght = 1;
+	Block* blocks = (Block*)MemAlloc(block_lenght * sizeof(Block));
+	if (!blocks) {
+		TraceLog(LOG_ERROR, "Failed to allocate memory for blocks");
+		CloseWindow();
+		return -1;
+	}
 
-	float lastFrameTime = (float) GetTime();
-	float currentFrameTime;
-	float accumulator = 0.0;
-
-	Color color = RED;
+	for (size_t i = 0; i < block_lenght; ++i) {
+		InitBlock(&blocks[i], window_width, window_height);
+	}
 
 	while (!WindowShouldClose()) {
 
-		currentFrameTime = (float) GetTime();
-		float dt = currentFrameTime - lastFrameTime;
-		lastFrameTime = currentFrameTime;
+		if (IsKeyDown(KEY_LEFT_SHIFT) && IsKeyPressed(KEY_EQUAL)) {
+			
+			block_lenght++;
+			blocks = (Block*)MemRealloc(blocks, block_lenght * sizeof(Block));
+			InitBlock(&blocks[block_lenght - 1], window_width, window_height);
 
-		accumulator += dt;
+		}
 
-		while (accumulator >= DELTA_TIME(fps)) {
+		float dt = GetFrameTime();
 
-			rect.x += (velocity_x * DELTA_TIME(fps));
-			rect.y += (velocity_y * DELTA_TIME(fps));
-
-			if (rect.x + BLOCK_SIZE > Window_width || rect.x < 0) {
-				velocity_x *= -1;
-				RandomizeColor(&color);
-			}
-
-			if (rect.y + BLOCK_SIZE > Window_height || rect.y < 0) {
-				velocity_y *= -1;
-				RandomizeColor(&color);
-			}
-
-			accumulator -= DELTA_TIME(fps);
-
+		for (size_t i = 0; i < block_lenght; ++i) {
+			UpdateBlock(&blocks[i], window_width, window_height, dt);
 		}
 
 		BeginDrawing();
@@ -70,11 +115,16 @@ int main() {
 		ClearBackground(BLACK);
 
 		DrawText(TextFormat("FPS: %d", GetFPS()), 0, 0, 16, WHITE);
-		DrawRectangleRec(rect, color);
+
+		for (size_t i = 0; i < block_lenght; ++i) {
+			RenderBlock(&blocks[i]);
+		}
 
 		EndDrawing();
 
 	}
+
+	MemFree(blocks);
 
 	CloseWindow();
 
